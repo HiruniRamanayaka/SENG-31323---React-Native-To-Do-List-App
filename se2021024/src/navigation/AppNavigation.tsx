1a671d17141965b3472f054bfb0c258cee820a11
HiruniRamanayaka <146801854+HiruniRamanayaka@users.noreply.github.com>
2025-04-19 19:14:43 +0530

    created navigation from opening page to home page

---
+++ se2021024/src/navigation/AppNavigation.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import IntroScreen from '../screens/Intro.screen'
+import HomeScreen from '../screens/Home.screen'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigation = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Intro" component={IntroScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  )
+}
+
+export default AppNavigation
---
+++ se2021024/src/screens/Intro.screen.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react'
+import { View, Text, ImageBackground, StyleSheet } from 'react-native' 
+
+const IntroScreen = ({navigation}: any) => {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigation.replace('Home')                      //Go to Home Screen after 2 seconds
+        }, 10000);
+        return () => clearTimeout(timer);                        // Cancel the timer if screen is closed early
+    }, [])
+
+  return (
+    <ImageBackground
+      source={require('../assets/i2.png')}
+      style={styles.background}
+      resizeMode="cover"
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    
+  },
+});
+
+export default IntroScreen
